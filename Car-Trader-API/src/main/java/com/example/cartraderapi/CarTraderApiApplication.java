e2e3207d2f553110cfe6b8cb670701f4d94a5381
tienhung79 <ngotienhung2000@gmail.com>
2023-07-13 09:45:20 +0700

    up

---
+++ Car-Trader-API/src/main/java/com/example/cartraderapi/CarTraderApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartraderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTraderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarTraderApiApplication.class, args);
+    }
+
+}
---
+++ Car-Trader-API/src/test/java/com/example/cartraderapi/CarTraderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cartraderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTraderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
