ab15e0453d81760136fc342bfef2f786121deb5b
tienhung79 <121491382+tienhung79@users.noreply.github.com>
2023-07-31 10:38:50 +0700

    Delete Car-Trader-API directory

--- Car-Trader-API/src/main/java/com/example/cartraderapi/service/account/IAccountService.java
+++
@@ -1,8 +0,0 @@
-package com.example.cartraderapi.service.account;
-
-import com.example.cartraderapi.model.Account;
-
-public interface IAccountService {
-    Account findAccountByNameAccount(String name);
-    Account saveAccount(Account account);
-}
